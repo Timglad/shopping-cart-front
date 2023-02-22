2abda07034a0ee4ca0daa7bd57ef6c6a208803bd
Tim_Glad <timothyboi64@gmail.com>
2023-02-23 01:38:02 +0200

    added archive function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,23 @@
 import { Alert } from "react-bootstrap"
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import axios from "axios";
+
 
 
 
 function Product({product}) {
+  const handleAddToCart = () => {
+    axios.post(`http://localhost:8000/cart/`, {
+      product: product.id,
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div>
         <h3>{product.name}</h3>
@@ -12,8 +25,10 @@ function Product({product}) {
         <Alert key="info">{product.price}</Alert>
         {/* <img src={'https://shopping-k6qe.onrender.com/static'+product.image} alt={product.name}/> */}
         <img src={'http://localhost:8000/static'+product.image} alt={product.name}/>
-        <Button variant="warning">Add to cart</Button>
-        <Link to={`/product/${product.id}`}>delete product</Link>
+        <Button variant="warning" onClick={handleAddToCart}>Add to cart</Button>
+        <Link to={`/product/${product.id}`}>
+        <Button variant="primary">View</Button>
+        </Link>
     </div>
   )
 }
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,35 +1,48 @@
 import React from 'react'
 import axios from "axios";
-import {useParams, Link } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 
-const SingleProduct = ({products}) => {
+const SingleProduct = ({ products }) => {
     const handleAddToCart = () => {
         axios.post(`http://localhost:8000/cart/`, {
-          product: id,
+            product: id,
         })
-          .then((response) => {
-            console.log(response);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      };
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    const handleRemoveProduct = () => {
+        axios.delete(`http://localhost:8000/product/` + id + "/")
+            .then((response) => {
+                 console.log(response);
+                
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
     const { id } = useParams();
     const product = products.find((p) => p.id === parseInt(id));
     if (!product) {
         return <div>Could'nt fetch product</div>;
-      }
-  return (
-    <div >
-      <h3>{product.name}</h3>
-        <p>{product.description}</p>
-        {/* <img src={'https://shopping-k6qe.onrender.com/static'+product.image} alt={product.name}/> */}
-        <img src={'http://localhost:8000/static'+product.image} alt={product.name}/>
-        <Button variant='warning' onClick={handleAddToCart}>Add to cart</Button>
-        <Link to={`/products`}>Return to products</Link>
-    </div>
-  )
+    }
+    return (
+        <div >
+            <h3>{product.name}</h3>
+            <p>{product.description}</p>
+            {/* <img src={'https://shopping-k6qe.onrender.com/static'+product.image} alt={product.name}/> */}
+            <img src={'http://localhost:8000/static' + product.image} alt={product.name} />
+            <Button variant='warning' onClick={handleAddToCart}>Add to cart</Button>
+            <Link to={`/products`}>
+                <Button variant='primary'>Return to products</Button>
+            </Link>
+            <Button variant='danger' onClick={handleRemoveProduct}>Delete</Button>
+        </div>
+    )
 }
 
 export default SingleProduct
