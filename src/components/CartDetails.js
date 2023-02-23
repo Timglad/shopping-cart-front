7bd92e8f9a3d1b6d265e959a8a5f1d29cce4eb68
John Doe <yuliagub@gmail.com>
2023-02-23 14:56:58 +0200

    bootstrap2

--- src/components/CartDetails.js
+++ src/components/CartDetails.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import { Alert } from "react-bootstrap"
+import axios from "axios";
 import Button from 'react-bootstrap/Button';
-<<<<<<< HEAD
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 
 function CartDetails( {item} ){
-=======
-import axios from "axios";
->>>>>>> 3859beaba8610c693f7e62c74ab9d40a2831b964
 
 
-const CartDetails = ({ item }) => {
     console.log(item)
     const handleRemoveitem = () => {
         axios.delete(`http://localhost:8000/cart/` + item.id + "/")
@@ -26,9 +21,8 @@ const CartDetails = ({ item }) => {
             });
     };
     return (
-<<<<<<< HEAD
         <>
-        return (
+        
             <Row xs={1} md={2} className="g-4">
               {Array.from({ length: 1 }).map((_, idx) => (
                 <Col>
@@ -38,7 +32,7 @@ const CartDetails = ({ item }) => {
                       <Card.Title>{item.product.name}</Card.Title>
                       <Card.Text>
                       {item.product.description}
-                        <Button variant='danger' onClick={()=>deleteHandler(item.id)} className="btn btn-danger btn-sm">Delete</Button>
+                        <Button variant='danger' onClick={()=>handleRemoveitem(item.id)} className="btn btn-danger btn-sm">Delete</Button>
                       </Card.Text>
                     </Card.Body>
                   </Card>
@@ -49,18 +43,6 @@ const CartDetails = ({ item }) => {
           );
         }
     
-=======
-        <div>
-            <h3>{item.id}</h3>
-            <p>{item.product.name}</p>
-            <Alert key="info">{item.product.price}</Alert>
-            <h3>{item.product.description}</h3>
-            <img src={'http://localhost:8000/static' + item.product.image} alt={item.product.name} />
-            <Button variant='danger' onClick={handleRemoveitem} className="btn btn-danger btn-sm">Delete</Button>
-        </div>
-    )
-}
->>>>>>> 3859beaba8610c693f7e62c74ab9d40a2831b964
 export default CartDetails
 
 
