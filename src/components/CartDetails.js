3859beaba8610c693f7e62c74ab9d40a2831b964
Tim_Glad <timothyboi64@gmail.com>
2023-02-23 13:59:06 +0200

    remove item from cart function

--- src/components/CartDetails.js
+++ src/components/CartDetails.js
@@ -1,33 +1,32 @@
 import React from 'react'
 import { Alert } from "react-bootstrap"
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import {useParams} from 'react-router-dom';
 import axios from "axios";
-import { useState, useEffect } from "react";
-
-function CartDetails( {item} ){
 
 
-    async function deleteHandler(id){
-        let res=await fetch("http://localhost:8000/cart/"+id,{
-        method : 'DELETE'
-    });
-    res = await res.json();
-    
-    console.warn(res)}
+const CartDetails = ({ item }) => {
+    console.log(item)
+    const handleRemoveitem = () => {
+        axios.delete(`http://localhost:8000/cart/` + item.id + "/")
+            .then((response) => {
+                console.log(response);
 
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
     return (
-    <div>
-        <h3>{item.id}</h3>
-        <p>{item.product.name}</p>
-        <Alert key="info">{item.product.price}</Alert>
-        <h3>{item.product.description}</h3>
-        <img src={'http://localhost:8000/static'+item.product.image} alt={item.product.name}/>
-        <Button variant='danger' onClick={()=>deleteHandler(item.id)} className="btn btn-danger btn-sm">Delete</Button>
-    </div>
+        <div>
+            <h3>{item.id}</h3>
+            <p>{item.product.name}</p>
+            <Alert key="info">{item.product.price}</Alert>
+            <h3>{item.product.description}</h3>
+            <img src={'http://localhost:8000/static' + item.product.image} alt={item.product.name} />
+            <Button variant='danger' onClick={handleRemoveitem} className="btn btn-danger btn-sm">Delete</Button>
+        </div>
     )
-    }   
+}
 export default CartDetails
 
 
