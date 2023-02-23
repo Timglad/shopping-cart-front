ea91d337a287a3ef7a2538de1357e817ba3e0071
John Doe <yuliagub@gmail.com>
2023-02-23 15:47:03 +0200

    bootstrap4

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,13 @@
 import CartDetails from "./CartDetails"
 import React from 'react'
-import { Col } from "react-bootstrap"
+import { Col, Row } from "react-bootstrap"
 
 function Cart({cart}) {
   return (
     <div> 
+    <Row>
     {cart.map(item => <Col xs={12} md={6} lg={4}><CartDetails key={item.id} item={item}/></Col>)}
+    </Row>
     </div>
   )
 }
